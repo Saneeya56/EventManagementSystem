6357fcd308fb6cd3c4604a4fb7c57ac14f9d9332
Saneeya56 <saneeyashaikh23@gmail.com>
2026-09-07 11:12:12 +0530

    v1.1

--- ems.py
+++ ems.py
@@ -1,6 +1,10 @@
-# Event Management System - Version 1.0
-# Feature: issue and return books
+# Library Management System - Version 1.1
+# Features: issue and return books, fine calculation
 def issue_book(book_id, member_id):
 print(&quot;Book&quot;, book_id, &quot;issued to member&quot;, member_id)
 def return_book(book_id):
 print(&quot;Book&quot;, book_id, &quot;returned&quot;)
+def calculate_fine(days_late, rate=5):
+fine = days_late * rate
+print(&quot;Fine = Rs.&quot;, fine)
+return fine

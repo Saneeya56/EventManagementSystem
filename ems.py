57634bb6c82f37980443ae5dc465c355e14aacbc
Saneeya56 <saneeyashaikh23@gmail.com>
2026-09-07 11:13:44 +0530

    v2.0

--- ems.py
+++ ems.py
@@ -1,5 +1,6 @@
-# Library Management System - Version 1.1
-# Features: issue and return books, fine calculation
+# Library Management System - Version 2.0
+# Features: issue/return books, fine calculation, online catalogue search
+catalogue = [&quot;DBMS&quot;, &quot;Operating Systems&quot;, &quot;Computer Networks&quot;]
 def issue_book(book_id, member_id):
 print(&quot;Book&quot;, book_id, &quot;issued to member&quot;, member_id)
 def return_book(book_id):
@@ -8,3 +9,8 @@ def calculate_fine(days_late, rate=5):
 fine = days_late * rate
 print(&quot;Fine = Rs.&quot;, fine)
 return fine
+def search_book(title):
+if title in catalogue:
+print(title, &quot;is available&quot;)
+else:
+print(title, &quot;not found&quot;)
